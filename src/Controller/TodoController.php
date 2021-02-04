663d86c775adb3f167c462b68f40a361d31e1a24
h.matsuo <matsuoshi@gmail.com>
2021-02-04 14:14:10 +0900

    index and create page

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,40 +2,59 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
 use App\Form\Type\TodoType;
+use App\Repository\TodoRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends Controller
 {
     /**
+     * @var TodoRepository
+     */
+    private $todoRepository;
+
+    public function __construct(TodoRepository $todoRepository)
+    {
+        $this->todoRepository = $todoRepository;
+    }
+
+    /**
      * @Route("/todo", name="todo")
+     * @Template("todo.html.twig")
      */
     public function index()
     {
-        return $this->render('todo.html.twig', [
-            'message' => 'hello',
-            'nums' => [1, 2, 3],
-            'flg' => true,
-        ]);
+        $todoList = $this->todoRepository->findBy([], ['id' => 'desc']);
+
+        return [
+            'todoList' => $todoList
+        ];
     }
 
     /**
      * @Route("/todo/create", name="todo_create")
      * @Template("create.html.twig")
+     * @param Request $request
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function create(Request $request)
     {
-        dump($request);
-
-        $form = $this->createForm(TodoType::class);
+        $form = $this->createForm(TodoType::class, new Todo());
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // do something
+            $todo = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($todo);
+            $entityManager->flush();
+
+            $this->addFlash('success', '登録しました。');
+
+            return $this->redirectToRoute('todo_create');
         }
 
         return [
--- src/Form/Type/TodoType.php
+++ src/Form/Type/TodoType.php
@@ -2,9 +2,11 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TodoType extends AbstractType
@@ -16,6 +18,13 @@ class TodoType extends AbstractType
             'constraints' => [
                 new NotBlank(),
             ]
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Todo::class
         ]);
     }
 }
